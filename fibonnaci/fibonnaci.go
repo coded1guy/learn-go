37691ca2c8d20443b1f950b5787dc63b946a01b7
coded1guy <grggmmnl@gmail.com>
2024-09-26 15:49:19 +0100

    feat: added a fibonnaci term generator

--- fibonnaci/fibonnaci.go
+++ fibonnaci/fibonnaci.go
@@ -1,6 +1,8 @@
 package fib
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func fibonnaci() func() int {
 	var num, fib_1, fib_2 = 0, 0, 1
@@ -19,8 +21,23 @@ func fibonnaci() func() int {
 }
 
 func Run() {
-	f := fibonnaci()
-	for i := 0; i < 10; i++ {
-		fmt.Println(f())
+	var num int = 0
+	fmt.Print("Enter a positive integer: ")
+	_, err := fmt.Scan(&num)
+	if err != nil {
+		fmt.Println("Error reading input:", err)
+		return
 	}
+
+	if num <= 0 {
+		fmt.Println("The number must be positive.")
+		return
+	}
+
+	fib_term := make([]int, num)
+	f := fibonnaci()
+	for i := 0; i < num; i++ {
+		fib_term[i] = f()
+	}
+	fmt.Printf("The first %d fibonnaci terms are: %v\n", num, fib_term)
 }
